9daf25e321b8088038d921cc712df97a1d77c89b
LENAERTS Tom <t.lenaerts@laposte.net>
2021-01-09 15:42:54 +0100

    Added indestructible blocks
    Added a bool for indetructible blocks and renamed sample scene.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -9,20 +9,25 @@ public class Block : MonoBehaviour
     public static event ScoreUp UpTHeScore;
 
     [SerializeField] int BlockHealth;
+
+    [SerializeField] bool Indestructible;
     #endregion
 
 
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
-        if (BlockHealth > 1)
+       if (Indestructible == false)
         {
-            BlockHealth = BlockHealth - 1;
-        }
+            if (BlockHealth > 1)
+            {
+                BlockHealth = BlockHealth - 1;
+            }
 
-        else
-        {
-            UpTHeScore?.Invoke();
-            Destroy(gameObject);
+            else
+            {
+                UpTHeScore?.Invoke();
+                Destroy(gameObject);
+            }
         }
     }
 }
